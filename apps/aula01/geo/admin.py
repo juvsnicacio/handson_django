2c567199ec86628835a681f5fecab46d58be1c2b
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-03-08 18:01:02 -0300

    upping

--- apps/aula01/geo/admin.py
+++ apps/aula01/geo/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
+from .models import UF, Municipio
 
-# Register your models here.
+
+@register(UF)
+class UFAdmin(ModelAdmin):
+    list_display = ('sigla', 'nome', 'codigo')
+
+
+@register(Municipio)
+class MunicipioAdmin(ModelAdmin):
+    list_display = ('codigo', 'uf', 'nome')
+    list_editable = ('uf', 'nome')
+    list_filter = ('uf__nome', )
+    search_fields = ('nome', 'codigo')
